194b1fe7f95e0f5450f793fc2ec52d74bf4af1d4
Changyoon Yang <ckddbs@gmail.com>
2019-03-06 15:53:03 +0900

    android/iOS sample

---
+++ Android sample/CupixWebPlayer/app/src/main/java/com/example/cupixwebplayer/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.cupixwebplayer;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.webkit.ConsoleMessage;
+import android.webkit.JavascriptInterface;
+import android.widget.Button;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // enable full screen
+
+        setContentView(R.layout.activity_main);
+
+        final WebView webView = findViewById(R.id.web);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebContentsDebuggingEnabled(true); // mobile webView debugging in Chrome
+        webView.setWebChromeClient(new WebChromeClient() {
+            public boolean onConsoleMessage(ConsoleMessage cm) {
+                Log.d("console log", "[" + cm.messageLevel() + "]" + cm.message() + " LN:" + cm.lineNumber() + " SRC: " + cm.sourceId());
+                return true;
+            }
+        });
+
+        // webView.loadUrl("http://players.cupix.com/p/ruHO8MyM");
+        webView.loadUrl("http://player-stage.cupix.com/p/r8qivuws");
+
+//        class CupixEventListener {
+//            @JavascriptInterface
+//            public void update(String eventType, String eventName, String arg) {
+//                Log.d("event log", "[CUPIX-EVENT][" + eventType + "] " +  eventName + ": " + arg);
+//            }
+//        }
+//        webView.addJavascriptInterface(new CupixEventListener(), "cupixEventListener");
+
+        class CupixEventListenerV2 {
+            @JavascriptInterface
+            public void update(String sender, String type, String args, String ver, String playerId, String caller) {
+                Log.d("event log", "[CUPIX-EVENT-V" + ver + "]" + sender + " - " + type + ": " + args+ ": " + playerId + ": " + caller);
+            }
+        }
+        webView.addJavascriptInterface(new CupixEventListenerV2(), "cupixEventListenerV2");
+
+        if (webView != null) {
+            final Button button = findViewById(R.id.send_event_button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        JSONObject args = new JSONObject();
+                        args.put("pos", new JSONArray(new Double [] {0.0, 0.0, 0.0}));
+                        args.put("up", new JSONArray(new Double [] {0.0, 0.0, 1.0}));
+                        args.put("lookat", new JSONArray(new Double [] {0.0, 1.0, 0.0}));
+                        args.put("fov", 60.);
+
+                        JSONObject event = new JSONObject();
+                        event.put("ver", "2");
+                        event.put("caller", "iOS");
+                        event.put("sender", "Camera");
+                        event.put("type", "Change");
+                        event.put("args", args);
+
+                        webView.evaluateJavascript("window.postMessage(" + event.toString() + ", '*')", null);
+                    }
+                    catch (JSONException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ iOS sample/CupixWebPlayer/CupixWebPlayer/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  CupixWebPlayer
+//
+//  Created by Changyoon Yang on 05/03/2019.
+//  Copyright © 2019 Cupix. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKScriptMessageHandler {
+
+	var webView: WKWebView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+
+		let webCfg:WKWebViewConfiguration = WKWebViewConfiguration()
+		let userController:WKUserContentController = WKUserContentController()
+		// userController.add(self, name: "cupixEventHandler")
+		userController.add(self, name: "cupixEventHandlerV2")
+		webCfg.userContentController = userController
+
+		self.webView = WKWebView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height), configuration: webCfg)
+		// let url = URL(string: "http://players.cupix.com/p/ruHO8MyM")
+		let url = URL(string: "http://player-dev.cupix.com/p/5Brgte3Z")
+		webView.load(URLRequest(url:url!))
+
+		self.view = self.webView
+
+		self.addSendEventButton();
+	}
+
+	func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+		if (message.name == "cupixEventHandler") {
+			let msg = message.body as! NSDictionary
+			let type = msg["eventType"] as! String
+			let eventName = msg["eventName"] as! String
+			let eventArg = msg["arg"] as! String
+			print("[CUPIX-EVENT][\(type)] \(eventName): \(eventArg)")
+		}
+		else if (message.name == "cupixEventHandlerV2") {
+			let msg = message.body as! NSDictionary
+			func parse(_ key: String) -> Any {
+				return msg[key] ?? "{}";
+			}
+			let ver = parse("ver")
+			let sender = parse("sender")
+			let type = parse("type")
+			let args = parse("args")
+			let pid = parse("player_id")
+			let caller = parse("caller")
+			print("[CUPIX-EVENT-V\(ver)]\(sender) - \(type): \(args): \(pid): \(caller)")
+		}
+	}
+
+	func addSendEventButton() {
+		let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+		button.center.x = self.view.center.x
+		button.center.y = self.view.center.y + 100
+		button.layer.cornerRadius = 10
+		button.backgroundColor = .orange
+		button.setTitle("Reset View", for: .normal)
+		button.addTarget(self, action: #selector(self.sendEventButtonAction), for: .touchUpInside)
+		self.view.addSubview(button)
+	}
+
+	func sendEventToPlayer(event: [String: Any]) {
+		guard let data = try? JSONSerialization.data(withJSONObject: event, options: []) else {
+			print("Failed to serialize JSON", event)
+			return
+		}
+		if let eventStr = String(data: data, encoding: String.Encoding.utf8) {
+			self.webView.evaluateJavaScript("window.postMessage(\(eventStr), '*')",
+				completionHandler: { (output, error) in if error != nil { print(error!) } })
+		} else {
+			print("Failed to stringify JSON", event)
+		}
+	}
+
+	@objc func sendEventButtonAction(sender: UIButton!) {
+		self.sendEventToPlayer(event: [
+			"ver": "2",
+			"caller": "iOS",
+			"sender": "Camera",
+			"type": "Change",
+			"args": [
+				"pos": [0.0, 0.0, 0.0],
+				"up": [0.0, 0.0, 1.0],
+				"lookat": [0.0, 1.0, 0.0],
+				"fov": 60.0
+			]
+		])
+	}
+
+}
+
