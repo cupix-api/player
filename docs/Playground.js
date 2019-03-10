fe726e5c9300b463c52cda0d5411d11f82a50bbf
Jim Anderson <48199038+JimAnderson2040@users.noreply.github.com>
2019-03-09 18:53:57 -0800

    Add files via upload

---
+++ docs/Playground.js
@@ -0,0 +1,141 @@
+$(document).ready(function() {
+    if ( WEBGL.isWebGLAvailable() === false ) {
+        document.body.appendChild( WEBGL.getWebGLErrorMessage() );
+        return;
+    }
+    
+    //
+    // Listeing events from the embedded Cupix Player
+    //
+    window.addEventListener("message", event => {
+        // Check if the version of API is 2
+        if(event.data.ver != "2") return;
+
+        const event_sender = event.data.sender;
+        const event_type = event.data.type;
+        const event_args = event.data.args;
+        const event_args_str = JSON.stringify(event_args);
+
+        // Show the event info in the left console
+        App.leftConsole.innerHTML = 
+        "<span class='prefix'>Sender:</span>" + event_sender + App.CR +
+        "<span class='prefix'>Type:</span>" + event_type + App.CR + 
+        "<span class='prefix'>Args:</span>" + event_args_str;
+
+        switch(event_sender){
+            case "Loader":
+                if(event_type == "Started"){
+                    // Loading is just started
+                }else if(event_type == "Progress"){
+                    // Loading is making progresses
+                }else if(event_type == "Completed"){
+                    // Loading is completed
+                    // Dlone args object and build the 3D tour structure
+                    model = new TourModel(JSON.parse(event_args_str));
+                    container = App.playerContainer;
+                    vc = new SceneViewController(model,container);
+                    app = new App( model, vc);
+                    App.viewController.render();
+                }
+            break;
+            case "ObjectHandler":
+                App.viewController.handleObjectEventsFromPlayer(event_type, event_args);
+            break;
+            case "TransitHandler":
+                if(event_type == "PanoStarted"){
+                }else if(event_type == "PanoCompleted"){
+                    if(App.viewController)
+                        App.viewController.activePano = event_args.id;
+                }else if(event_type == "GroupCompleted"){
+                    if(App.viewController)
+                        App.viewController.activeGroup = event_args.id;
+                }
+            break;
+            case "Camera": 
+                if(event_type == "Changed"){
+                    if(App.viewController)
+                        App.viewController.updatePanoCamera(event_args);
+                }
+            break;
+            default: break;
+        }
+    }, false);
+
+    //
+    // UI handlers
+    //
+    $("#btn-open").on('click', event => {
+        event.preventDefault();
+        let url = $("#txt-player-url").val();
+        if(url == "" ) return;
+        App.openURL(url);
+    });
+
+    $(document).on('keyup', event => {
+        // If the Del key is pressed
+        if(event.keyCode == 46) {
+            App.removeFocusedObj();
+        }
+    });
+
+    $("#btn-sync").on('click', event => App.toggleCamera())
+    
+    // Command -> Parent event calls
+    
+    $("#btn-show-help").on('click', event => {
+        App.toggleToastMessageVisibility();
+    });
+    
+    $("#drop-command").on('click', '.dropdown-item', event => {
+        let cmd_str = event.currentTarget.text;
+        switch(cmd_str){
+            case "Add Box": 
+                App.printToConsole("Adding a randomly-sized box at the center of the scene.");
+                App.addBox(); 
+            break;
+            case "Add Pushpin":
+            // Dialog window will show up
+            break;
+            case "Dist b/w Panos":
+                App.printToConsole("Adding a dimension between two selected panos.");
+                App.addDim();
+            break;
+            case "Measure - Pick Floor Method":
+                App.printToConsole("Measure a dist in the Player by picking points on the floor.");
+                App.measureDim("pick_on_floor");
+            break;
+            case "Measure - Two Pano Method":
+                App.printToConsole("Measure a dist in the Player by picking points using 2-pano matching point method.");
+                App.measureDim("pick_2_panos");
+            break;
+            case "Cancel":
+                App.printToConsole("Cancel measurement.");
+                App.cancelCommand();
+            break;
+        }
+    });
+    $('#add-pushpin-modal').on('hide.bs.modal', function (event) {
+        if($(document.activeElement) && $(document.activeElement)[0].id=='btn-add-pushpin'){
+            App.printToConsole( "Adding a randomly-positioned pushpin");
+
+            let title = $('#pushpin-title')[0].value;
+            let desc = $('#pushpin-body')[0].value;
+            let show_leg = ($('#show-leg')[0].value == "on" ? true : false);
+            let auto_anchor = ($('#auto-anchor')[0].value == "on" ? true : false);
+
+            App.addPushpin(title, desc, show_leg, auto_anchor);
+        }
+    });
+
+    // Group changed
+    $('#setion-list').on('click', '.dropdown-item', event => {
+        event.preventDefault();
+        
+        $("#txt-active-section").val(event.currentTarget.text);
+        
+        let index = parseInt(event.currentTarget.id.substring(App.sectionDivPrefix.length));
+
+        App.changeGroup(index);
+    });
+
+});
---
+++ docs/ViewController.js
@@ -0,0 +1,377 @@
+class SceneViewController{
+    constructor(tour_model, div_container){
+        this._tour_model = tour_model;
+        this._scene = this._tour_model.scene;
+        this._container = div_container;
+        this._handled_object_hash_table = {};
+
+        this._active_group_index = 0;
+        this.initGroups();
+        
+        this._renderer = new THREE.WebGLRenderer( { antialias: true } );
+        this._renderer.setPixelRatio( window.devicePixelRatio );
+        this._renderer.setSize( div_container.clientWidth, div_container.clientHeight );
+        this._renderer.shadowMap.enabled = true;
+        this._renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+        // Enable key up/down event in the canvas
+        this._renderer.domElement.tabIndex = 1;
+
+        this.activeCamera = this.activeGroup.camera;
+        var controls = new THREE.TrackballControls( this.activeCamera, div_container);
+        controls.rotateSpeed = 1.0;
+        controls.zoomSpeed = 1.2;
+        controls.panSpeed = 0.8;
+        controls.noZoom = false;
+        controls.noPan = false;
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
+        controls.keys = [ 65, 83, 68 ];
+        this._controls = controls;
+        this._controls.addEventListener( 'change', this.render );
+
+        // Pano-Camera: A camera attachable to a pano. There is just one pano-camera.
+        // Pano-Camera uses the global coordiante system.
+        this._pano_camera = new THREE.PerspectiveCamera( 60, div_container.clientWidth / div_container.clientHeight, 0.1, 1.0 );
+        this._pano_camera_helper = new THREE.CameraHelper( this._pano_camera );
+        this._pano_camera.add(new THREE.PointLight( 0x999999 ));
+
+        var first_person = new THREE.OrbitControls(this._pano_camera, div_container);
+        first_person.enabled = false;
+        this._first_person_controls = first_person;
+        
+        this._scene.add( this._pano_camera );
+        this._scene.add( this._pano_camera_helper );
+    
+        div_container.appendChild( this._renderer.domElement );
+        this._renderer.autoClear = false;
+        window.addEventListener( 'resize', this.onWindowResize.bind(this), false );
+        
+        this._ray_caster = new THREE.Raycaster();
+        this._mouse_pos = new THREE.Vector2();
+
+        // Mouse event handling
+        this.default = 0
+        this.measureStarted = 1;
+        this.measurePickcedFirst = 2;
+
+        this._command_mode = this.default;
+
+        this._renderer.domElement.addEventListener( 'mousemove', event => {
+            event.preventDefault();
+            this._mouse_pos.x = ( event.offsetX / this._renderer.domElement.width ) * 2 - 1;
+            this._mouse_pos.y = - ( event.offsetY / this._renderer.domElement.height) * 2 + 1;
+
+            this.checkMouseOnPano();
+        }, false );
+
+        this._renderer.domElement.addEventListener( 'mousedown', event => {
+            var event_to_send;
+
+            if(this._pano_on_mouse){
+                App.printToConsole(this._pano_on_mouse.name + " is selected.");
+                
+                switch(this._command_mode){
+                case this.measureStarted:
+                    this._pano_picked = this._pano_on_mouse;
+                    this._command_mode = this.measurePickedFirst;
+                    App.printToConsole("Select one more pano.");
+                break;
+                case this.measurePickedFirst:
+                    if(this._pano_picked == this._pano_on_mouse){
+                        App.printToConsole("Two panos are the same. Select a different pano.");
+                        return;
+                    }
+                    let col = App.getRandomColor();
+                    var start = [0,0,0];
+                    var end = [0,0,0];
+                    App.assignVec2Array3D (start, this._pano_picked.position);
+                    App.assignVec2Array3D (end, this._pano_on_mouse.position);
+                    this.scene.add(App.makeLineObject(
+                        this._pano_picked.position.clone(),
+                        this._pano_on_mouse.position.clone(), 
+                        new THREE.LineBasicMaterial( { color: col} ), 
+                        true, true ));
+
+                    this.render();
+
+                    event_to_send = App.createEventToPlayer ("ObjectHandler", "AddDimension")            
+                    event_to_send.args = {
+                        name : this._pano_on_mouse.name + "-" + this._pano_picked.name,
+                        geom : {
+                            start: start,
+                            end: end
+                        }
+                    }
+                    App.emitEventToPlayer(event_to_send);
+
+                    let dx = Math.abs(start[0] - end[0]).toPrecision(3);
+                    let dy = Math.abs(start[1] - end[1]).toPrecision(3);
+                    let dz = Math.abs(start[2] - end[2]).toPrecision(3);
+                    let dd = Math.sqrt(dx*dx + dy*dy + dz*dz).toPrecision(3);
+                    App.printToConsole("Distane is: " + dd + " (" + dx + ", " + dy + ", " + dz +")");
+                    this._pano_picked = null;
+                    this._command_mode = this.default;
+                break;
+                default:
+                    this._pano_picked = null;
+                    event_to_send = App.createEventToPlayer ("TransitHandler", "ChangePano")            
+                    event_to_send.args = {
+                        id : this._pano_on_mouse.cupixID,
+                        name : this._pano_on_mouse.name
+                    }
+                    App.emitEventToPlayer(event_to_send);
+                }
+            }
+        }, false );
+    }
+
+    render() {
+        if(this.renderer)
+            this.renderer.render( this.scene, this.activeCamera );
+        
+        if(this.controls)
+            this.controls.update();
+    }
+
+    onWindowResize() {
+        this.activeCamera.aspect = this.container.clientWidth / this.container.clientHeight;
+        this.activeCamera.updateProjectionMatrix();
+
+        if(this.renderer)
+            this.renderer.setSize( this.container.clientWidth, this.container.clientHeight );
+        
+        this.controls.handleResize();
+        this.render();
+    }
+
+    highlightObject(obj){
+        obj.material.emissive.setHex( 0x333333 );
+    }
+
+    deHighlightObject(obj){
+        obj.material.emissive.setHex( 0 );
+    }
+
+    checkMouseOnPano(){
+        let cam = this.activeCamera;
+        cam.updateMatrixWorld();
+        this._ray_caster.setFromCamera( this._mouse_pos, cam );
+        var intersects = this._ray_caster.intersectObjects( this.activeGroup.children, true );
+        var found = false;
+        for(var i=0;i<intersects.length;++i){
+            if(intersects[i].object.cupixID != undefined){
+                if ( this._pano_on_mouse != intersects[i].object ) {
+                    this._pano_on_mouse = intersects[i].object;
+                    this.highlightObject(this._pano_on_mouse);
+                }
+                $('canvas').css('cursor','pointer');
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            if ( this._pano_on_mouse )
+                this.deHighlightObject( this._pano_on_mouse )
+            this._pano_on_mouse = null;
+            $('canvas').css('cursor','default');
+        }
+
+        this.render();
+    }
+
+    handleObjectEventsFromPlayer(event_type, event_args){
+        var customObj = App.searchCustomObj(event_args.name);
+        
+        switch(event_type){
+        case "Selected":
+            if(event_args.type=="Dimension" && this._handled_object_hash_table[event_args.id] == undefined){
+                let col = 0x00ff00;
+                const start = event_args.geom.start;
+                const end = event_args.geom.end;
+                this.scene.add(App.makeLineObject(
+                        new THREE.Vector3(start[0], start[1], start[2]),
+                        new THREE.Vector3(end[0], end[1], end[2]), 
+                        new THREE.LineBasicMaterial( { color: col} ), 
+                        true, true ));
+
+                this._handled_object_hash_table[event_args.id] = true;
+                this.render();
+            }
+            break;
+        case "Focused":
+            if( customObj != null ){
+                this.highlightObject (customObj.sceneObject);
+                this.render();
+            }
+            break;
+        case "Blurred":
+            if( customObj != null ){
+                this.deHighlightObject (customObj.sceneObject);
+                this.render();
+            }
+            break;
+        case "Added":
+            // Object was created from parent
+            // Set the id generated in the player 
+            if( customObj != null )
+                customObj.cupixID = event_args.id;
+            break;
+        default: break;
+        }
+    }
+
+    clear(){
+        $(this._renderer.domElement).remove();
+        window.removeEventListener( 'resize', this.onWindowResize );
+        App.clearThree(this._scene);
+        this._renderer = null;
+        this._scene = null;
+    }
+
+    initGroups(){
+        for(let i = 0;i<this._tour_model._section_groups.length;++i){
+            const section_group = this._tour_model._section_groups[i];
+            var section_dropdown = $(".dropdown-menu#setion-list");
+            let sectino_div_id = App.sectionDivPrefix+i;
+            section_dropdown.append("<a class='dropdown-item' id='" + sectino_div_id + "' href='#'>" + section_group.name + "</a>");
+            if(i==0){
+                $("#txt-active-section").val(section_group.name);
+            }else{
+                section_group.visible = false;
+            }
+        }
+    }
+
+    toggleCamera(){
+        if(this.activeCamera == this._pano_camera){
+            this.activeCamera = this.activeGroup.camera;
+            this._pano_camera.far = 1.0;
+            this._pano_camera_helper.visible = true;
+            this.activeGroup.axesHelper.visible = true;
+
+            this.controls.enabled = true;
+            this.panoControls.enabled = false;
+        }else{
+            this.activeCamera = this._pano_camera;
+            this._pano_camera.far = 100.0;
+            this._pano_camera_helper.visible = false;
+            this.activeGroup.axesHelper.visible = false;
+
+            this.controls.enabled = false;
+
+            // TODO: not working
+            //this.panoControls.enabled = true;
+        }
+        
+        this.render();
+    }
+
+    updatePanoCameraHelper(){
+        this._pano_camera.updateProjectionMatrix();
+        this._pano_camera_helper.update();
+        
+        this.render();
+    }
+
+    updatePanoCamera(player_pano_camera){
+        //
+        // player_pano_camera is in the global cooridnate sytem
+        //
+        // No need to update player_pano_camera.pos 
+        // (currently pano-cam is attached to the ativePano)
+        // assignArray2Vec3D(this._pano_camera.position, player_pano_camera.pos);
+        
+        App.assignArray2Vec3D(this._pano_camera.up, player_pano_camera.up);
+        this._pano_camera.lookAt(
+            player_pano_camera.pos[0] + player_pano_camera.lookat[0], 
+            player_pano_camera.pos[1] + player_pano_camera.lookat[1], 
+            player_pano_camera.pos[2] + player_pano_camera.lookat[2]);
+        
+        this._pano_camera.fov = player_pano_camera.fov;
+        this.updatePanoCameraHelper();
+    }
+
+    get activeGroup(){
+        return this._tour_model._section_groups [ this._active_group_index ];
+    }
+
+    set activeGroup(id){
+        for(let i=0;i<this._tour_model._section_groups.length;++i){
+            if(this._tour_model._section_groups[i].cupixID == id){
+                this.activeGroupIndex = i;
+                break;
+            }
+        }
+    }
+
+    get activeGroupIndex(){
+        return this._active_group_index;
+    }
+
+    set activeGroupIndex(i){
+        if(i==NaN || i < 0 || i > this._tour_model._section_groups.length) return;
+        if( this._active_group_index == i) return;
+        
+        this.activeGroup.visible = false;
+    
+        this._active_group_index = i;
+        let group = this._tour_model._section_groups[i];
+        group.visible = true;
+
+        this.activeCamera = group.camera;
+        
+        this.controls.object = this.activeCamera;
+        this.controls.enabled = true;
+        this.panoControls.enabled = false;
+        
+        this.render();
+    }
+    
+    get activeCamera(){
+        return this._active_camera;
+    }
+
+    set activeCamera(c){
+        c.aspect = this._container.clientWidth / this._container.clientHeight;
+        this._active_camera = c;
+    }
+
+    get activePano(){
+        return this._active_pano;
+    }
+
+    set activePano(pano_id){
+        this._active_pano = this._tour_model._pano_hash_table[pano_id];
+        App.assignVec2Vec3D( this._pano_camera.position, this._active_pano.position);
+        this.updatePanoCameraHelper();
+    }
+
+    get scene(){
+        return this._scene;
+    }
+
+    get renderer(){
+        return this._renderer;
+    }
+
+    get controls(){
+        return this._controls;
+    }
+
+    get panoControls(){
+        return this._first_person_controls;
+    }
+
+    get container(){
+        return this._container;
+    }
+
+    get commandMode(){
+        return this._command_mode;
+    }
+
+    set commandMode(m){
+        this._command_mode = m
+    }
+}
